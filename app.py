556d7ebf10165fc76761695bf56c8a3f2b761e89
Guewen Baconnier <guewen@gmail.com>
2014-09-12 22:04:40 +0200

    add a hook

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask
+import json
+from flask import Flask, request
+from prettyprint import prettyprint as pp
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return 'Nothing there'
+
+
+@app.route('/hook', methods=['POST'])
+def hook():
+    data = json.loads(request.data)
+    event = request.headers['X-GitHub-Event']
+    pp(event)
+    pp(data)
+    return 'OK'
