d54fb0fd3f58d0fb050e0c6adad524c5a4b1ef2b
Guewen Baconnier <guewen@gmail.com>
2014-09-14 20:49:47 +0200

    Display an history of last pushes

--- app.py
+++ app.py
@@ -5,9 +5,23 @@ from pprint import pprint as pp
 app = Flask(__name__)
 
 
+MAX_HISTORY = 50
+history = []
+
+
+def add_to_history(push):
+    history = history[:MAX_HISTORY]
+    history.append(push)
+
+
 @app.route('/')
 def index():
-    return 'Nothing there'
+    template = ('<html><head><title>OCA CLA</title></head>'
+                '<body><table>%s</table></body></html>')
+    page = template % '\n'.join(['<tr><td>%s</td><td>%s</td></tr>' %
+                                 (push['ref'], push['after']) for push
+                                 in history])
+    return page
 
 
 @app.route('/hook', methods=['POST'])
@@ -17,4 +31,6 @@ def hook():
     pp(request.headers)
     pp(event)
     pp(data)
+    if event == 'push':
+        add_to_history(data)
     return 'OK'
