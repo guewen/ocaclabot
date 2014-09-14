4de80aa1978f4c93344a101595697fd9aacd01f3
Guewen Baconnier <guewen@gmail.com>
2014-09-14 20:59:23 +0200

    new class for GitHub pushes

--- app.py
+++ app.py
@@ -14,13 +14,29 @@ def add_to_history(push):
     history.append(push)
 
 
+class GitHubPush(object):
+
+    def __init__(self, data):
+        self.data = data
+
+    def repo_name():
+        return self.data.get('repository', {}).get('full_name', '')
+
+    def owner_name():
+        return self.data.get('repository', {}).get('owner', {}).get('name', '')
+
+    def commits():
+        return self.data.get('commits', [])
+
+
 @app.route('/')
 def index():
     template = ('<html><head><title>OCA CLA</title></head>'
                 '<body><table>%s</table></body></html>')
+
     page = template % '\n'.join(['<tr><td>%s</td><td>%s</td></tr>' %
-                                 (push['ref'], push['after']) for push
-                                 in history])
+                                 (push.repo_name(),
+                                  push.owner_name()) for push in history])
     return page
 
 
@@ -32,5 +48,5 @@ def hook():
     pp(event)
     pp(data)
     if event == 'push':
-        add_to_history(data)
+        add_to_history(GitHubPush(data))
     return 'OK'
